624f4dc9e5ca8efbe9028939d7b828c44f8e375c
cmsy86 <cmsy86@naver.com>
2021-08-08 16:20:43 +0900

    0805_Login_Delonghi

---
+++ script/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    //nav#gnb
+    $(".mainLi").mouseover(function(){
+        $(this).children("ul").stop().show();
+        $("#subMenuBG").stop().show();
+    });
+    $(".mainLi").mouseout(function(){
+        $(this).children("ul").stop().hide();
+        $("#subMenuBG").stop().hide();
+    });
+
+});
